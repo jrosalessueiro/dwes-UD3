929aa1105c36e00148e3c369c88f1db77d39800f
joserosalessueiro <jrsueiro@yahoo.es>
2023-11-27 20:21:42 +0100

    Creada la BBDD ytabla da error

--- ud03/tienda/dar_de_alta.php
+++ ud03/tienda/dar_de_alta.php
@@ -12,16 +12,35 @@
 <body>
     <h1>Alta de usuario </h1>
     <?php
+        /*if (isset($_POST["nombre"], $_POST["apellidos"])) {
+            $nombre = $_POST['nombre'];
+            $apellidos = $_POST['nombre'];
+
+            $conexion = get_conextion();
+
+            $conextion->executeQuery("INSERT INTO usuarios (nombre, apellidos) VALUES (" . $nombre . ", " . $apellidos . ")")
+        }
+        */
+        //echo 'HOLA ' . ($_POST['fname'] ?? null) . '!';
+        
         //Comprobar se veñen datos polo $_POST
         //Conexión
         //Seleccionar bd
         //Executar o INSERT
+        //$connection->execute("INSERT INTO usuarios (nombre, apellidos) VALUES $POST[09")
     ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
     </script>
 
     <p>Formulario de alta</p>
+    <form action="dar_de_alta.php" method="post">
+    <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <input type="submit" value="Submit">
+    </form>
     <!-- o "action" chama a dar_de_alta.php de xeito reflexivo-->
     
     <footer>
--- ud03/tienda/lib/base_datos.php
+++ ud03/tienda/lib/base_datos.php
@@ -1,24 +1,41 @@
 <?php
 
 function get_conexion(){
-    //Código para establecer la conexión
-
-    $conexion = new mysqli('db','root','test','dbname');
+    $conexion = new mysqli('localhost','root','1234');
     // Comprobar la conexión
-    $error =$conexion->connect_errno;
-    if($error !=null){
+    $error = $conexion->connect_error;
+    if($error != null){
         die("Fallo en la conexión: ".$conexion->connect_error. "con número". $error);
     }
-    echo "Conexión correcta";
-    //Cerrar la conexión
-    $conexion-> close();
-
-    //return $conexion;
+    
+    return $conexion;
 }
 
 function seleccionar_bd_tienda($conexion){
     $conexion->select_db("tienda");
+    
 }
-function crear_bd_tienda(){
-    //Código para creación de bd
+function crear_bd_tienda($conexion){
+    $sql = "CREATE DATABASE IF NOT EXISTS tienda";
+    if($conexion->query($sql)){
+        echo "Base de datos creada correctamente";
+    }else{
+        echo "Error a la hora de crear la base de datos".$conexion->error;
+    }
+}
+
+function crear_tabla_usuario($conexion){
+    $sql = "CREATE TABLE IF NOT EXISTS usuarios(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nombre VARCHAR(50) NOT NULL,
+        apellido VARCHAR(100) NOT NULL,
+        edad INT,
+        provincia VARCHAR(50)";
+
+if($conexion->query($sql)){
+    echo "Tabla creada correctamente";
+}else{
+    echo "Error a la hora de crear la tabla".$conexion->error;
+}
+    
 }
