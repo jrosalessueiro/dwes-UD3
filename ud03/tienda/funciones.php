480dbdbf2db115fe8cc0e6ac03240641da277a00
joserosalessueiro <jrsueiro@yahoo.es>
2024-01-14 22:29:09 +0100

    Ejercicio Ficheros ud04. funciones.php

--- ud03/tienda/funciones.php
+++ ud03/tienda/funciones.php
@@ -1,22 +1,39 @@
 <?php
 function comprobaciones($foto)
 {
+    //Las imagenes se guardaran en la carpeta uploads con los permisos adecuados
     $target_dir = "uploads/";
     //Este es el archivo con su nombre
     $target_file = $target_dir . basename($_FILES["pphoto"]["name"]);
+    //Esta es la extensión del fichero
     $tipo_fichero = pathinfo($target_file, PATHINFO_EXTENSION);
 
+    //Vemos si existe e fichero
     if (!file_exists($target_file)) {
-        if ($_FILES["pphoto"]["size"] > 50000) {
-            if ($tipo_fichero == "png" || $tipo_fichero == "jpg" || $tipo_fichero == "jpeg" || $tipo_fichero == "gif") {
-                move_uploaded_file($_FILES["pphoto"]["tmp_name"], $target_file);
-            } else {
-                echo "El tipo de fichero no es válido. Únicamente se acepta .jpg, .jpeg, .gif, o .png";
-            }
+        if (compruebaExtension($tipo_fichero) == true && compruebaTamanho($foto) == true) {
+            move_uploaded_file($_FILES["pphoto"]["tmp_name"], $target_file);
         } else {
-            echo "El fichero es demasiado grande. Máximo 50MB.";
+            echo "El fichero ya existe.";
         }
+    }
+}
+
+function compruebaExtension($tipo_fichero)
+{
+    //Comprobamos si la extensión está dentro de las que se aceptan
+    if ($tipo_fichero == "png" || $tipo_fichero == "jpg" || $tipo_fichero == "jpeg" || $tipo_fichero == "gif") {
+        return true;
     } else {
-        echo "El fichero ya existe.";
+        echo "El tipo de fichero no es válido. Únicamente se acepta .jpg, .jpeg, .gif, o .png";
+    }
+}
+
+function compruebaTamanho($foto)
+{
+    //Comprobamos si el tamaño es menor de 50MB
+    if ($_FILES["pphoto"]["size"] > 50000) {
+        return true;
+    } else {
+        echo "El fichero es demasiado grande. Máximo 50MB.";
     }
 }
