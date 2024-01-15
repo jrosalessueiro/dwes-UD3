f9bd5240d3fb57910151391105371f962dc0e233
joserosalessueiro <jrsueiro@yahoo.es>
2024-01-15 22:13:09 +0100

    empezando el ajercicio login ud04

---
+++ ud04/tienda_login/dar_de_alta.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tienda IES San Clemente </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Alta de usuario </h1>
+    <?php
+    if (isset($_POST["fname"], $_POST["password"], $_POST["lname"], $_POST["age"], $_POST["state"])) {
+        $nombre = $_POST['fname'];
+        $_password = $_POST["password"];
+        $apellidos = $_POST['lname'];
+        $edad = $_POST['age'];
+        $provincia = $_POST['state'];
+
+        include("lib/base_datos.php");
+        $conexion = get_conexion();
+        seleccionar_bd_tienda($conexion);
+
+        insertar_usuario($conexion, $password, $nombre, $apellidos, $edad, $provincia);
+    }
+
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
+
+    <p>Formulario de alta</p>
+    <form action="dar_de_alta.php" method="post">
+        <label for="fname">Nombre:</label>
+        <input type="text" id="fname" name="fname" required><br><br>
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" name="password" required><br><br>
+        <label for="lname">Apellidos:</label>
+        <input type="text" id="lname" name="lname" required><br><br>
+        <label for="age">Edad:</label>
+        <input type="text" id="age" name="age" required><br><br>
+        <label for="state">Provincia:</label>
+        <input type="text" id="state" name="state" required><br><br>
+
+        <input type="submit" value="Guardar">
+    </form>
+    <!-- o "action" chama a dar_de_alta.php de xeito reflexivo-->
+
+    <footer>
+        <p>
+            <a href='index.php'>Página de inicio</a>
+        </p>
+    </footer>
+</body>
+
+</html>
---
+++ ud04/tienda_login/login.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+
+function comprobar_usuario($nombre, $pass)
+{
+    include("lib/base_datos.php");
+    $conexion = get_conexion();
+    seleccionar_bd_tienda($conexion);
+
+    $user = usuariobd($conexion, $nombre);
+    $contrasenha = passbd($conexion, $pass);
+
+    if ($nombre == $user && $pass == $contrasenha) {
+        return true;
+    } else {
+        echo "La contraseña no coincide con la del usuario $nombre.";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Login de tienda IES San Clemente</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <h2>Login de usuarios registrados:</h2><br>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        Usuario: <input type="text" id="usuario" name="usuario"><br><br>
+        Contraseña: <input type="password" id="password" name="password"><br><br>
+        <input type="submit">
+    </form>
+</body>
+
+</html>
