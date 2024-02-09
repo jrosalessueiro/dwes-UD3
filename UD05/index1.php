6914bafec249c1af47d834be52fafbd69db6d184
joserosalessueiro <jrsueiro@yahoo.es>
2024-02-09 17:31:13 +0100

    Completado Ejercicio 1 UD05

--- UD05/index1.php
+++ UD05/index1.php
@@ -2,52 +2,56 @@
 //Ejercicio 1. Contacto
 
 //Crea una clase Contacto en un fichero Contacto.php, con las siguientes propiedades privadas: nombre, apellido y número de teléfono.
-
 class Contacto
 {
     private $nombre;
     private $apellido;
     private $numeroTelefono;
 
-
-//Define un constructor con 3 argumentos, que asigne los valores a las propiedades.
-function contacto($nombre, $apellido, $numeroTelefono){
-
-}
-
-
-
-
-    //Genera los getters y los setters para todas las propiedades y el método muestraInformacion() que imprima los valores de las propiedades.
-
-    function set_nombre($nombre){
+    //Define un constructor con 3 argumentos, que asigne los valores a las propiedades.
+    function __construct($nombre, $apellido, $numeroTelefono)
+    {
         $this->nombre = $nombre;
-    }
-    function set_apellido($apellido){
         $this->apellido = $apellido;
-    }
-    function set_numeroTelefono($numeroTelefono){
         $this->numeroTelefono = $numeroTelefono;
     }
-    function get_nombre($nombre){
+
+    //Agrega un método __destruct() a la clase, que muestra en pantalla el objeto que se está destruyendo.
+    function __destruct()
+    {
+        echo "El contacto {$this->nombre} {$this->apellido} se está destruyendo.\n";
+    }
+
+    //Genera los getters y los setters para todas las propiedades y el método muestraInformacion() que imprima los valores de las propiedades.
+    function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+    function setApellido($apellido)
+    {
+        $this->apellido = $apellido;
+    }
+    function setNumeroTelefono($numeroTelefono)
+    {
+        $this->numeroTelefono = $numeroTelefono;
+    }
+    function getNombre()
+    {
         return $this->nombre;
     }
-    function get_apellido($apellido){
+    function getApellido()
+    {
         return $this->apellido;
     }
-    function get_numeroTelefono($numeroTelefono){
+    function getNumeroTelefono()
+    {
         return $this->numeroTelefono;
     }
-    function muestraInformacion(){
-        echo Contacto->get_nombre();
+    function muestraInformacion()
+    {
+        echo 'Nombre: ' . $this->nombre . "\n";
+        echo 'Apellido: ' . $this->apellido . "\n";
+        echo 'Teléfono: ' . $this->numeroTelefono . "\n";
     }
 }
-
-
-//Define un constructor con 3 argumentos, que asigne los valores a las propiedades.
-
-
-//Crea 3 objetos de la clase Contacto en un fichero ContactoTest.php, asigna valores a todas sus propiedades y muéstrelos a continuación, primero con los métodos get() 
-//y luego con el método muestraInformacion().
-
-//Agrega un método __destruct() a la clase, que muestra en pantalla el objeto que se está destruyendo.
+?>
